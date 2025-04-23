c4fb1885e00597368a4aae1837350f77b1443c3d
KarthikRaja017 <karthikrajayuvaraj16@gmail.com>
2025-04-23 18:46:58 +0530

    Initial commit

--- web/app/services/apiConfig.ts
+++ web/app/services/apiConfig.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/server/api",
+  baseURL: "https://dockly.onrender.com/server/api",
   headers: {
     "Content-Type": "application/json",
   },
