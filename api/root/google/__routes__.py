e45902ad04e9e6ce5a331b6698a21d011a04240c
KarthikRaja017 <karthikrajay.cc@gmail.com>
2025-06-02 21:46:47 +0530

    modified dockly and added new db

--- api/root/google/__routes__.py
+++ api/root/google/__routes__.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # from .models import AddGoogleCalendar, GetGoogleCalendarEvents, GoogleCallback
 # from . import google_api
 
@@ -6,7 +5,6 @@
 # google_api.add_resource(AddGoogleCalendar, "/add-googleCalendar")
 # google_api.add_resource(GoogleCallback, "/auth/callback/google")
 # google_api.add_resource(GetGoogleCalendarEvents, "/get/calendar/events")
-=======
 from .models import AddGoogleCalendar, GetGoogleCalendarEvents, GoogleCallback
 from . import google_api
 
@@ -14,4 +12,10 @@ from . import google_api
 google_api.add_resource(AddGoogleCalendar, "/add-googleCalendar")
 google_api.add_resource(GoogleCallback, "/auth/callback/google")
 google_api.add_resource(GetGoogleCalendarEvents, "/get/calendar/events")
->>>>>>> deb0ba0 (modified dockly and added new db)
+# from .models import AddGoogleCalendar, GetGoogleCalendarEvents, GoogleCallback
+# from . import google_api
+
+
+# google_api.add_resource(AddGoogleCalendar, "/add-googleCalendar")
+# google_api.add_resource(GoogleCallback, "/auth/callback/google")
+# google_api.add_resource(GetGoogleCalendarEvents, "/get/calendar/events")
